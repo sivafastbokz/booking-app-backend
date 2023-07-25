57327ea15ec3db5d6cf7904dbde672a02fc8e12d
sivafastbokz <sivaharshanfastbokz@gmail.com>
2023-07-25 20:00:10 +0530

    updated

--- src/model/schema.js
+++ src/model/schema.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const CustomerSchema = new mongoose.Schema({
     customerName:{
         type:String,
-        unique:true,
         required:true
     },
     customerPhoneNo:{
